6a8a7682d0a6bcd2d52f3e276b3748f1f8852489
Gašper Pintar <pintargasper1@gmail.com>
2024-07-14 15:58:05 +0200

    Updated favicon.ico Updated logo192.png Updated logo512.png Updated Index.js   -Added instructions for "what you need" Updated manifest.json  -Changed short_name from React App to Card generator  -Changed name from Create React App Sample to Card generator (from excel to cards) Updated package.json  -Change version from 0.1.o to 1.0.1

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,3 +1,5 @@
+import {Link} from "react-router-dom";
+
 const Index = () => {
     return (
         <div className="container">
@@ -12,12 +14,32 @@ const Index = () => {
                 </div>
             </section>
 
+            <section id="what-you-need" className="bg-light section">
+                <div className="section-content">
+                    <h3>Kaj potrebuješ</h3>
+                    <ul>
+                        <li>Excel datoteko <a
+                            href={"https://github.com/pintargasper/CardGenerator/releases/download/pre-release/Cards.xlsx"}
+                            target={"_blank"} rel="noreferrer">Cards.xlsx (Vzorčna datoteka)</a> -> pridobljeno iz strani <a
+                            href={"https://github.com/pintargasper/CardGenerator/releases"}
+                            target={"_blank"} rel="noreferrer">Github</a>
+                        </li>
+                        <li>Mapo slik <a
+                            href={"https://github.com/pintargasper/CardGenerator/releases/download/pre-release/images.zip"}
+                            target={"_blank"} rel="noreferrer">images (Vzorčna mapa)</a> -> pridobljeno iz
+                            strani <a href={"https://github.com/pintargasper/CardGenerator/releases"}
+                                target={"_blank"} rel="noreferrer">Github</a></li>
+                        <li>Datoteka (Predloga) -> pridobljeno iz <Link to={"./create"}>predloge</Link></li>
+                    </ul>
+                </div>
+            </section>
+
             <section id="formats" className="bg-light section">
                 <div className="section-content">
                     <h3>Podprti formati</h3>
                     <ul>
                         <li>
-                            A4 -> 3 x 3 slik na eno stran. Velikost karte: <b>Širina:</b> 240px; <b>Višina:</b> 332px
+                        A4 -> 3 x 3 slik na eno stran. Velikost karte: <b>Širina:</b> 240px; <b>Višina:</b> 332px
                         </li>
                         <li>
                             13x18 -> 2 x 2 slik na eno stran. Velikost karte: <b>Širina:</b> 240px; <b>Višina:</b> 332px
@@ -38,12 +60,12 @@ const Index = () => {
                             <b>Uporaba komponente:</b>
                         </p>
                         <ul>
-                            <li>title={"<b>Naslov</b>"}</li>
-                            <li>progress={"<b>Število</b>"}</li>
-                            <li>textColor={"<b>Barva besedila</b>"}</li>
-                            <li>loadingBarColor={"<b>Barva vrstice</b>"}</li>
-                            <li>fontFamily={"<b>Pisava</b>"}</li>
-                            <li>fontSize={"<b>Velikost besedila</b>"}</li>
+                            <li>title=&#123;<b>&#34;Naslov&#34;</b>&#125;</li>
+                            <li>progress=&#123;<b>Število</b>&#125;</li>
+                            <li>textColor=&#123;<b>&#34;Barva besedila&#34;</b>&#125;</li>
+                            <li>loadingBarColor=&#123;<b>&#34;Barva vrstice&#34;</b>&#125;</li>
+                            <li>fontFamily=&#123;<b>&#34;Pisava&#34;</b>&#125;</li>
+                            <li>fontSize=&#123;<b>Velikost besedila</b>&#125;</li>
                         </ul>
                     </div>
                 </div>
