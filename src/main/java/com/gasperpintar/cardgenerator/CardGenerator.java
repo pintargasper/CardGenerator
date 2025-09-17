c88818ef35bf1184cf2d94afb9b56da1f96b5909
Gašper Pintar <pintargasper1@gmail.com>
2025-09-17 12:53:22 +0200

    Splash screen with progress bar and logo
    - Implemented splash screen with animated loading bar
    - Display application logo while loading
    - Prepare main stage after loading completes

--- src/main/java/com/gasperpintar/cardgenerator/CardGenerator.java
+++ src/main/java/com/gasperpintar/cardgenerator/CardGenerator.java
@@ -1,11 +1,18 @@
 package com.gasperpintar.cardgenerator;
 
 import com.gasperpintar.cardgenerator.utils.Utils;
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -13,18 +20,62 @@ import java.util.Objects;
 public class CardGenerator extends Application {
 
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(CardGenerator.class.getResource("layout/main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+    public void start(Stage primaryStage) throws IOException {
+        showSplashScreen(primaryStage);
+    }
 
-        Image image = new Image(Objects.requireNonNull(CardGenerator.class.getResourceAsStream("images/logo.png")));
-        stage.getIcons().add(image);
+    private void showSplashScreen(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(CardGenerator.class.getResource("layout/splash.fxml"));
+        VBox splashLayout = fxmlLoader.load();
 
-        stage.setTitle("Card Generator");
-        stage.setScene(scene);
+        Image iconImage = new Image(Objects.requireNonNull(
+                CardGenerator.class.getResourceAsStream("images/logo.png")));
 
-        Utils.stage = stage;
+        ProgressBar progressBar = (ProgressBar) splashLayout.lookup("#progressBar");
+        ImageView logoView = (ImageView) splashLayout.lookup("#logoView");
+        logoView.setImage(iconImage);
 
-        stage.show();
+        Stage splashStage = new Stage(StageStyle.UNDECORATED);
+        splashStage.getIcons().add(iconImage);
+        splashStage.setScene(new Scene(splashLayout));
+        splashStage.show();
+
+        new Thread(() -> {
+            try {
+                for (int i = 1; i <= 100; i++) {
+                    Thread.sleep(20);
+                    final double progress = i / 100.0;
+                    Platform.runLater(() -> progressBar.setProgress(progress));
+                }
+                Platform.runLater(() -> loadMainStage(primaryStage, splashStage, iconImage, splashLayout));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+    }
+
+    private void loadMainStage(Stage primaryStage, Stage splashStage, Image iconImage, VBox splashLayout) {
+        try {
+            FXMLLoader mainLoader = new FXMLLoader(CardGenerator.class.getResource("layout/main.fxml"));
+            Scene mainScene = new Scene(mainLoader.load(), 320, 240);
+
+            primaryStage.getIcons().add(iconImage);
+            primaryStage.setTitle("Card Generator");
+            primaryStage.setScene(mainScene);
+            primaryStage.setMaximized(true);
+
+            Utils.stage = primaryStage;
+
+            FadeTransition fadeOut = new FadeTransition(Duration.millis(500), splashLayout);
+            fadeOut.setFromValue(1.0);
+            fadeOut.setToValue(0.0);
+            fadeOut.setOnFinished(actionEvent -> {
+                splashStage.close();
+                primaryStage.show();
+            });
+            fadeOut.play();
+        } catch (IOException ioException) {
+            throw new RuntimeException(ioException);
+        }
     }
 }
